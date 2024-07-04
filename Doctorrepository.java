f8ae9afc24302f6f1d2fb0819d0f18976736cc22
Sathyaprakash42 <satyapotharaveni666@gmail.com>
2024-07-04 13:12:59 +0530

    Create Doctorrepository.java

---
+++ Doctorrepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    List<Doctor> findByCityAndSpeciality(String city, String speciality);
+}
