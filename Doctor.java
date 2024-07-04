ea98778adf64d45a11774d5bfb61a1ed889c644e
Sathyaprakash42 <satyapotharaveni666@gmail.com>
2024-07-04 13:10:45 +0530

    Create Doctor.java

---
+++ Doctor.java
@@ -0,0 +1,36 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3)
+    private String name;
+
+    @NotBlank
+    @Size(max = 20)
+    private String city;
+
+    @Email
+    private String email;
+
+    @Pattern(regexp = "\\d{10,}")
+    private String phoneNumber;
+
+    @NotBlank
+    private String speciality;
+
+    // getters and setters
+  }
